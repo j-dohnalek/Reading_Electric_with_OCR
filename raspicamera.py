bbe2097c1c755d1c6c74ab9dfa4acf249d225ad3
Ian Havelock <ian@morrolan.com>
2014-03-17 11:53:38 +0000

    Tweaked camera_settings in raspicamera.py

--- raspicamera.py
+++ raspicamera.py
@@ -14,22 +14,24 @@ def take_picture(imagename, resolution=None, camera_settings=None):
 
     if camera_settings is None:
         # camera settings
-        camera_settings = {'co': 80,  # contrast
+        _camera_settings = {'co': 80,  # contrast
                            'iso': 800,  # ISO
                            'em': 'night',  # exposure mode
                            'mm': 'matrix',  # metering mode
                            'ss': 150000,  # shutter speed
                            'awb': 'auto'}  # white balance
+    else:
+        _camera_settings = camera_settings
 
     with picamera.PiCamera() as camera:
         camera.resolution = (resolution[0], resolution[1])
         camera.start_preview()
-        camera.contrast = camera_settings['co']
-        camera.iso = camera_settings['iso']
-        camera.exposure_mode = camera_settings['em']
-        camera.meter_mode = camera_settings['mm']
-        camera.shutter_speed = camera_settings['ss']
-        camera.awb = camera_settings['awb']
+        camera.contrast = _camera_settings['co']
+        camera.iso = _camera_settings['iso']
+        camera.exposure_mode = _camera_settings['em']
+        camera.meter_mode = _camera_settings['mm']
+        camera.shutter_speed = _camera_settings['ss']
+        camera.awb = _camera_settings['awb']
         # Give the camera some time to adjust to conditions
         sleep(2)
         camera.capture(imagename)
