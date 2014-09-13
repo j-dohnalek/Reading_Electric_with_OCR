2c24c82e399fa568766f61989f78e2f5c5da7592
learn2develop <thinkabout3e@googlemail.com>
2014-09-13 23:39:56 +0100

    Create raspicamera.py

---
+++ lib/raspicamera.py
@@ -0,0 +1,51 @@
+import picamera
+import time
+image that you want to create
+    :param resolution:  image resolution to sa
+# Resolution of the image from the camera
+# better to keep is lower this will decrease
+# the processing time
+
+
+def take_picture(imagename, resolution=None, camera_settings=None):
+    """
+
+    :param imagename:  name of the ve as.  Default is 800x600
+    :param camera_settings:  the settings for the camera.  Defaults are:
+        camera_settings dict:   {
+                                'co': 80,        # contrast
+                                'iso': 800,      # ISO
+                                'em': 'night',   # exposure mode
+                                'mm': 'matrix',  # metering mode
+                                'ss': 150000,    # shutter speed
+                                'awb': 'auto'    # white balance
+                                }
+    """
+
+    if resolution is None:
+        resolution = [800, 600]
+
+    # there is a more resilient way of specifying this to make this more re-usable.
+    if camera_settings is None:
+        _camera_settings = {'co': 25,  # contrast
+                            'iso': 640,  # ISO
+                            'em': 'night',  # exposure mode
+                            'mm': 'spot',  # metering mode
+                            'sh': 90,
+                            'awb': 'auto'}  # white balance
+    else:
+        _camera_settings = camera_settings
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = (resolution[0], resolution[1])
+        camera.start_preview()
+        camera.contrast = _camera_settings['co']
+        camera.iso = _camera_settings['iso']
+        camera.exposure_mode = _camera_settings['em']
+        camera.meter_mode = _camera_settings['mm']
+        camera.sharpness = 100
+        camera.awb = _camera_settings['awb']
+        # Give the camera some time to adjust to conditions
+        time.sleep(2)
+        camera.capture(imagename)
+        camera.stop_preview()
