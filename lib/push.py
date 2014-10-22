d8c803bdf100b13d712d56fed45a355ff2870994
learn2develop <thinkabout3e@googlemail.com>
2014-10-22 17:39:21 +0100

    Update push.py

--- lib/push.py
+++ lib/push.py
@@ -1,117 +1,40 @@
-# IMPORTS ###################################################################
+# IMPORTS ########################
 
-from ConfigParser import SafeConfigParser
-import os
-import os.path
-
-# CONSTANTS #################################################################
-MAINDIR = '/home/shares/nas/smartCam/'
-
-MAIL = MAINDIR + 'conf/email.ini'
-USER = MAINDIR + 'users.ini'
-CONF = MAINDIR + 'conf/config.ini'
-
-# FUNCTIONS #################################################################
+import httplib
+import urllib
 
 
-def get_email_setting(section, option, _return=None):
-    """
-    :param section: section to retrieve from INI file
-    :param option: option to retrieve from INI file
+def send_message(message, title=None):
     """
 
-    parser = SafeConfigParser()
-    parser.read(MAIL)
+    Function sends push messages to the specified user
 
-    if _return is None:
-        return str(parser.get(section, option))
-    elif _return == 'int':
-        return parser.getint(section, option)
-
-
-def get_conf_setting(section, option, _return=None):
-    """
-    :param section: section to retrieve from INI file
-    :param option: option to retrieve from INI file
+    :param from_ip: senders IP address
+    :param message: Message to user
+    :param title: Title of the message
     """
 
-    parser = SafeConfigParser()
-    parser.read(CONF)
+    app_token = 'YOUR APP TOKEN'
+    usr_token = 'YOUR USER TOKEN'
+    #device = 'nexus5'
 
-    if _return is None:
-        return str(parser.get(section, option))
-    elif _return == 'int':
-        return parser.getint(section, option)
+    if title is None:
+        _title = "Smart Home Notification"
+    else:
+        _title = title
 
+    # Start your connection with the Pushover API server
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
 
-def get_user_info(num):
+    # Send a POST request in urlencoded json
+    conn.request("POST", "/1/messages.json",
+    urllib.urlencode({
+    "token": app_token,
+    "user": usr_token,
+    #"device": device,
+    "title": _title,
+    "message": message,
+    }), {"Content-type": "application/x-www-form-urlencoded"})
 
-    user = []
-    from ConfigParser import SafeConfigParser
-
-    parser = SafeConfigParser()
-    parser.read(USER)
-
-    section = "user" + str(num + 1)
-    if parser.has_section(section):
-        for name in parser.options(section):
-            user.append(parser.get(section, name))
-
-    return user
-
-
-def users_list():
-
-    _list = []
-
-    for i in range(10):
-        if len(get_user_info(i)) is not 0:
-            _list.append(get_user_info(i))
-
-    return _list
-
-
-def read_file(_file):
-    check_file_exists(_file)
-
-    f = open(_file, "r")
-    data = f.read()
-    f.close()
-
-    return data
-
-
-def write_to_file(_file, data):
-    check_file_exists(_file)
-
-    f = open(_file, "w")
-    f.write(data)
-    f.close()
-
-
-def check_file_exists(_file):
-    if not os.path.exists(_file):
-        f = open(_file, "w")
-        f.write('0')
-        f.close()
-
-
-def clean_directory(path):
-    """
-
-    :param _path:  path to clean
-    """
-
-    _file = os.listdir(path)
-
-    for i in range(0, len(_file)):
-        if os.path.exists(path + '/' + _file[i]):
-            os.remove(path + '/' + _file[i])
-
-
-def setup(path):
-    """ Set up the folder structure """
-    if not os.path.exists(path + get_conf_setting('general', 'temp')):
-        os.makedirs(path + get_conf_setting('general', 'temp'))
-    if not os.path.exists(path + get_conf_setting('general', 'cache')):
-        os.makedirs(path + get_conf_setting('general', 'cache'))
+    # Listen for any error messages or other responses
+    conn.getresponse()
